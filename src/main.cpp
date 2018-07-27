fb39b3db504efae675ebee703731e8ecaedc5d01
Joe Harding <joe.harding@us.ibm.com>
2018-07-27 17:03:04 -0500

    a bit clearer on vbo vs vao

--- src/main.cpp
+++ src/main.cpp
@@ -17,20 +17,6 @@
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
 
-struct BMP_DICT
-{
-  SDL_Surface* player;
-  SDL_Surface* enemy;
-};
-
-bool loadAssets (BMP_DICT* dict)
-{
-  SDL_Surface* red = SDL_LoadBMP("assets/red.bmp");
-  SDL_Surface* blue = SDL_LoadBMP("assets/blue.bmp");
-  dict -> player = blue;
-  dict -> enemy = red;
-  return true;
-}
 
 bool SetOpenGLAttributes ()
 {
@@ -52,29 +38,26 @@ bool SetOpenGLAttributes ()
 const uint32_t positionAttributeIndex = 0;
 const uint32_t uvAttributeIndex = 1;
 
-GLuint VertexArrayID;
 GLuint TextureID;
 GLuint programID;
-GLuint vertexbuffer;
-GLuint uvbuffer;
 
 glm::mat4 model;
 glm::mat4 view;
 glm::mat4 projection;
 glm::quat camera;
 
+GLuint vbo[2], vao[1];
 
 void GLInit ()
 {
-  glGenVertexArrays(1, &VertexArrayID);
-  glBindVertexArray(VertexArrayID);
-
-  std::cout << "VertexArrayID: " << VertexArrayID << std::endl;
-
   // Create and compile our GLSL program from the shaders
   programID = LoadShaders( "shaders/simple.vert", "shaders/simple.frag" );
 
-  static const GLfloat g_vertex_buffer_data[] = { 
+	glGenBuffers(2, vbo);
+	glGenVertexArrays(1, vao);
+	glBindVertexArray(vao[0]); 
+ 
+	GLfloat vertex_buffer_data[] = { 
     -1.0f, -1.0f, 0.0f,
      1.0f, -1.0f, 0.0f,
      1.0f,  1.0f, 0.0f,
@@ -83,13 +66,12 @@ void GLInit ()
     -1.0f,  1.0f, 0.0f
   };
 
-  glGenBuffers(1, &vertexbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_buffer_data), vertex_buffer_data, GL_STATIC_DRAW);
+  glVertexAttribPointer (positionAttributeIndex, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glEnableVertexAttribArray(positionAttributeIndex);
  
-  std::cout << "vertexbuffer: " << vertexbuffer << std::endl;
-
-  static const GLfloat uv_buffer_data[] = { 
+  GLfloat uv_buffer_data[] = {
     0.0f, 0.0f,
     1.0f, 0.0f,
     1.0f, 1.0f,
@@ -98,24 +80,18 @@ void GLInit ()
     0.0f, 1.0f
   };
 
-  glGenBuffers(1, &uvbuffer);
-  glBindBuffer(GL_ARRAY_BUFFER, uvbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(uv_buffer_data), uv_buffer_data, GL_STATIC_DRAW);
-  
-  std::cout << "uvbuffer: " << uvbuffer << std::endl;
+  glVertexAttribPointer (uvAttributeIndex, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glEnableVertexAttribArray(uvAttributeIndex);
 
-  // 90 degrees
-  projection = glm::perspective(
-    glm::radians(90.0f),
-    static_cast<float>(SCREEN_WIDTH) / static_cast<float>(SCREEN_HEIGHT),
-    0.0f,
-    100.0f
-  );
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	float aspect_ratio = static_cast<float>(SCREEN_WIDTH) / static_cast<float>(SCREEN_HEIGHT);
+  projection = glm::perspective (glm::radians (90.0f), aspect_ratio, 0.0f, 100.0f);
 
   model = glm::mat4(1.0f);
    
-  // You should probably use CSurface::OnLoad ... ;)
-  //-- and make sure the Surface pointer is good!
   SDL_Surface* Surface = SDL_LoadBMP ("assets/red.bmp");
    
   glGenTextures(1, &TextureID);
@@ -137,19 +113,17 @@ void GLInit ()
 void Render (SDL_Window* window)
 {
   // apply camera rotation to view matrix
-  glm::mat4 rotation_matrix = glm::toMat4(camera);
+  glm::mat4 rotation_matrix = glm::toMat4 (camera);
   view = view * rotation_matrix;
 
-  model = glm::mat4();
-  model = glm::translate( model, glm::vec3( 0.0f, 0.0f, -20.0f ));
-  model = glm::scale( model, glm::vec3( 10.0f, 10.0f, 0.0f ));
+  model = glm::mat4 ();
+  model = glm::translate (model, glm::vec3 (0.0f, 0.0f, -20.0f));
+  model = glm::scale (model, glm::vec3 (10.0f, 10.0f, 0.0f));
 
   glClearColor (0.5, 0.5, 0.5, 1.0);
   glClear (GL_COLOR_BUFFER_BIT);
 
   glUseProgram (programID);
-
-  glBindBuffer (GL_ARRAY_BUFFER, VertexArrayID);
 
   glUniformMatrix4fv (glGetUniformLocation (programID, "u_model"),
       1, GL_FALSE, glm::value_ptr (model));
@@ -158,17 +132,12 @@ void Render (SDL_Window* window)
   glUniformMatrix4fv (glGetUniformLocation (programID, "u_projection"),
       1, GL_FALSE, glm::value_ptr (projection));
   
-  glVertexAttribPointer (positionAttributeIndex, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-  glVertexAttribPointer (uvAttributeIndex, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
-  
-  glEnableVertexAttribArray(positionAttributeIndex);
+	glBindVertexArray (vao[0]); 
 
-  glDrawArrays(GL_TRIANGLES, 0, 6); // 3 indices starting at 0 -> 1 triangle
-
-  glDisableVertexAttribArray(positionAttributeIndex);
+  glDrawArrays (GL_TRIANGLES, 0, 6); // 3 indices starting at 0 -> 1 triangle
 
   // Swap our buffers to make our changes visible
-  SDL_GL_SwapWindow(window);
+  SDL_GL_SwapWindow (window);
 }
 
 int main(int argc, char* args[])
@@ -177,16 +146,14 @@ int main(int argc, char* args[])
   SDL_GLContext mainContext = NULL;
   SDL_Surface* redBlock = NULL;
 
-  if (SDL_Init(SDL_INIT_VIDEO) < 0)
+  if (SDL_Init (SDL_INIT_VIDEO) < 0)
   {
     fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
     return 1;
   }
   window = SDL_CreateWindow("earlyman",
           SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-          SCREEN_WIDTH, SCREEN_HEIGHT,
-          SDL_WINDOW_OPENGL
-          );
+          SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL);
   if (window == NULL)
   {
     fprintf(stderr, "could not create window: %s\n", SDL_GetError());
@@ -203,20 +170,9 @@ int main(int argc, char* args[])
 
   GLInit ();
 
-  /*
-  BMP_DICT image_dict;
-  loadAssets (&image_dict);
-
-  int red_change = 1;
-  SDL_Rect red_loc = {0,0,600,400};
-  SDL_Rect blue_loc = {0,0,600,400};
-  */
-
-  //Main loop flag
   bool quit = false;
   bool red_dead = false;
 
-  //Event handler
   SDL_Event e;
 
   //While application is running
